12a04ebaa5d80c29ecb6e8e12598b042ae274836
b4dragon <dragon.b4@gmail.com>
2019-04-04 16:38:36 +0900

    test to read version through am

--- src/pm/core.clj
+++ src/pm/core.clj
@@ -1,7 +1,8 @@
 (ns pm.core
   (:require [aleph.http :as http]
-            [manifold.stream :as s]))
-
+            [manifold.stream :as s]
+            [cognitect.transit :as t]))
+            
 (defn foo
   "I don't do a whole lot."
   [x]
@@ -10,5 +11,8 @@
 
 ;;testing server
 (def ws @(http/websocket-client "ws://0.0.0.0:8080"))
+@(s/put! ws "read-+8-+288")
+(def a @(s/take! ws))
+(close ws)
 
-@(s/put! ws "hi")
+
